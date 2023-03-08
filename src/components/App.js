c0f8ff4ed8e8173f3be0b0ccfa1cd1768ceabd7e
Ghimirevivek <stephengmre@gmail.com>
2023-03-08 12:34:21 +0530

    corrected

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,43 @@
 import React from 'react'
-import '../styles/App.css';
-
+import '../styles/App.css'
+import { useState } from 'react'
 const App = () => {
-
+  const [expenseList, setExpenseList] = useState([])
+  const [totalExpense, setTotalExpense] = useState(0)
+  const handleAddExpense = () => {
+    const expenseInput = document.getElementById('expense-input')
+    const expense = expenseInput.value.trim()
+    if (expense !== '') {
+      const espenseArr = expense.split('-')
+      const itemName = espenseArr[0]
+      const itemPrice = parseFloat(espenseArr[1])
+      if (!isNaN(itemPrice)) {
+        const newExpenseList = [...expenseList, { itemName, itemPrice }]
+        setExpenseList(newExpenseList)
+        setTotalExpense(totalExpense + itemPrice)
+        expenseInput.value = ''
+      }
+    }
+  }
   return (
     <div id="main">
-      <input id="expense-input" />
-      <button id="expense-button">Add Expense</button>
+      <input
+        id="expense-input"
+        placeholder="Enter item name and price e.g. Groceries - 50.00"
+      />
+      <button id="expense-button" onClick={handleAddExpense}>
+        Add Expense
+      </button>
       <div id="expense-list">
+        {expenseList.map((expense, index) => (
+          <div key={index}>
+            {expense.itemName} - {expense.itemPrice.toFixed(2)}
+          </div>
+        ))}
       </div>
-      <div id="total-expense">
-        Total Expense: 0
-      </div>
+      <div id="total-expense">Total Expense: {totalExpense.toFixed(2)}</div>
     </div>
   )
 }
 
-
-export default App;
+export default App
